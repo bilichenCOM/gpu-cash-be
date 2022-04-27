64b4673b37bb30958a4d7dd94bbf40d3eb499dff
mbilichenko <bilichenko.mykhailo@gmail.com>
2022-04-28 01:05:22 +0300

    fix primary key generation in liquibase scripts;

--- src/main/java/com/bilichenko/gpucashdemo/controller/OrderController.java
+++ src/main/java/com/bilichenko/gpucashdemo/controller/OrderController.java
@@ -3,11 +3,14 @@ package com.bilichenko.gpucashdemo.controller;
 import com.bilichenko.gpucashdemo.model.Order;
 import com.bilichenko.gpucashdemo.service.OrderService;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@RestController()
+@RequestMapping("/orders")
 @CrossOrigin("http://localhost:4200")
 public class OrderController {
 
@@ -17,6 +20,7 @@ public class OrderController {
         this.orderService = orderService;
     }
 
+    @GetMapping
     public List<Order> getAll() {
         return orderService.getAll();
     }
