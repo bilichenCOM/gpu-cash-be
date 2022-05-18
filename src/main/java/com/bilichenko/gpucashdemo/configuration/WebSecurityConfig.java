cb2138518554153b279a7ceee51b9e0e7ddab20c
mbilichenko <bilichenko.mykhailo@gmail.com>
2022-05-18 17:26:23 +0300

    adjust spring security;

--- src/main/java/com/bilichenko/gpucashdemo/configuration/WebSecurityConfig.java
+++ src/main/java/com/bilichenko/gpucashdemo/configuration/WebSecurityConfig.java
@@ -9,6 +9,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                        .anyRequest().permitAll()
+                .and().csrf().disable();
         http.requiresChannel()
                 .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
                 .requiresSecure();
