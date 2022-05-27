f9733430e015d64361d17fcd860de69303914207
mbilichenko <bilichenko.mykhailo@gmail.com>
2022-05-28 00:38:45 +0300

    add vrm models;

--- src/main/java/com/bilichenko/gpucashdemo/model/DrMos.java
+++ src/main/java/com/bilichenko/gpucashdemo/model/DrMos.java
@@ -1,4 +1,17 @@
 package com.bilichenko.gpucashdemo.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "dr_mos")
 public class DrMos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String name;
 }
